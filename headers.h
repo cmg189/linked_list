65cc4651e66991cd69c4be3565563d42f7ebbc71
cmg189 <cmg189@outlook.com>
2023-01-12 13:32:03 -0600

    Employee class defined

--- headers.h
+++ headers.h
@@ -1,2 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Employee
+{
+public:
+    // constructors
+    Employee();
+    Employee(string name, int id, string position, float salary);
+
+    // accessors
+    string get_name();
+    int get_id();
+    string get_position();
+    float get_salary();
+
+    // mutators
+    void set_name(string name);
+    void set_id(int id);
+    void set_position(string position);
+    void set_salary(float salary);
+
+private:
+    string name;
+    int id;
+    string position;
+    float salary;
+};
