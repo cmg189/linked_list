44859b42ecd6b75608bbe2e31085b2ac860517c2
cmg189 <cmg189@outlook.com>
2023-01-12 13:55:57 -0600

    format fix

--- headers.h
+++ headers.h
@@ -2,28 +2,27 @@
 #include <string>
 using namespace std;
 
-class Employee
-{
-public:
-    // constructors
-    Employee();
-    Employee(string name, int id, string position, float salary);
+class Employee {
+    public:
+        // constructors
+        Employee();
+        Employee(string name, int id, string position, float salary);
 
-    // accessors
-    string get_name();
-    int get_id();
-    string get_position();
-    float get_salary();
+        // accessors
+        string get_name();
+        int get_id();
+        string get_position();
+        float get_salary();
 
-    // mutators
-    void set_name(string name);
-    void set_id(int id);
-    void set_position(string position);
-    void set_salary(float salary);
+        // mutators
+        void set_name(string name);
+        void set_id(int id);
+        void set_position(string position);
+        void set_salary(float salary);
 
-private:
-    string name;
-    int id;
-    string position;
-    float salary;
+    private:
+        string name;
+        int id;
+        string position;
+        float salary;
 };
